305dfd94e49cd3e0b4676ee678974d7c81987c17
Leandro Ardissone <lardissone@gmail.com>
2019-11-21 17:15:14 -0300

    Moves database information to dev only

--- minesweeper/minesweeper/settings/base.py
+++ minesweeper/minesweeper/settings/base.py
@@ -110,10 +110,3 @@ SWAGGER_SETTINGS = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
